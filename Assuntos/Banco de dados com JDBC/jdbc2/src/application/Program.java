1d7fa8a3e10a7334ed917fd5f9e423ed9050463e
DevEren <dev.erenildo@gmail.com>
2024-02-16 15:53:53 -0300

    inserindo dados

--- Assuntos/Banco de dados com JDBC/jdbc2/src/application/Program.java
+++ Assuntos/Banco de dados com JDBC/jdbc2/src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.Db;
 
@@ -11,25 +14,47 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null;
 		
 		try {
 			conn = Db.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from department");
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES "
+					+ "(?, ?, ?, ?, ?)", 
+					Statement.RETURN_GENERATED_KEYS
+			);
 			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id") + " : " + rs.getString("Name"));
+			st.setString(1, "ErenNildo");
+			st.setString(2, "developer.erenildo@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("22/12/2003").getTime()));
+			st.setDouble(4, 5000.0);
+			st.setInt(5, 1);
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Pronto ! ID = " + id);
+				}
+			}else {
+				System.out.println("Nenhuma linha foi alterada !");
 			}
-		} catch(SQLException e) {
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(ParseException e) {
 			e.printStackTrace();
 		} finally {
-			Db.closeResultSet(rs);
 			Db.closeStatement(st);
 			Db.closeConnection();
 		}
 		
 	}
+		
 }
