7cf5fda8ab5ae1a3ab7611924fb9480d9277c672
DevEren <dev.erenildo@gmail.com>
2024-02-29 08:56:10 -0300

    conectando MYSQL

--- Assuntos/spring boot/aula-spring-jpa/src/main/java/dio/aula/model/User.java
+++ Assuntos/spring boot/aula-spring-jpa/src/main/java/dio/aula/model/User.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "tab_user")
 public class User {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "user_id")
 	private Integer id;
 	
